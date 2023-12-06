0ddeb2cfffe61b1a072331201be0070abcd93100
ivan.ivanov <ivan.ivanov@sourcico.com>
2023-12-06 12:34:00 +0100

    AOF on existing repo

---
+++ Day_1/app.js
@@ -0,0 +1,62 @@
+import * as fs from 'fs';
+
+let dummy = fs.readFileSync("./dummy.txt")
+let dummyParsed = dummy.toString("utf-8")
+
+// console.log("BEFORE" ,dummyParsed)
+
+// console.log("BEFORE" ,typeof(dummyParsed) )
+
+// console.log(dummyParsed)
+
+// console.log(dummy)
+
+// console.log("Type of parsed data:", typeof(dummyParsed))
+
+ const splitted = dummyParsed.split('\n')
+
+const onlyNumbers = splitted.map((arraySplited) => {
+  const numbersArray = arraySplited
+    .split('')
+    .filter((char) => !isNaN(char))
+    .join('');
+
+    return numbersArray.replace(/\r/g, '');
+});
+
+// console.log("HERE" ,onlyNumbers)
+
+// console.log('Splitted', splitted)
+
+
+const onlyFirstAndSecondNumber = onlyNumbers.map((string) => {
+
+  // const filteredOnlyFirstAndSecond = string
+  // .split('')
+  
+  return `${string.charAt(0)}${string.charAt(string.length-1)}`
+
+})
+
+// console.log(onlyFirstAndSecondNumber)  /* extracted only first and second number*/
+
+
+let convertedTonumber = onlyFirstAndSecondNumber.map((string) => {
+
+  return +string
+})
+
+
+console.log(convertedTonumber) /**all strings converted to numbers */
+
+let sumOfAlls = 0
+
+convertedTonumber.map((num) => {
+
+  
+  sumOfAlls += num
+
+  return sumOfAlls
+})
+
+// console.log("heres",sumOfAlls)
